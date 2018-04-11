a2dbe2294f6b48bc181082f12e9c5047ffaac9db
yucfeng <lxfyc314@163.com>
2018-04-11 17:12:04 +0800

    initalize

---
+++ src/main/java/com/yucfeng/yucfeng/YucfengApplication.java
@@ -0,0 +1,12 @@
+package com.yucfeng.yucfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YucfengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YucfengApplication.class, args);
+	}
+}
